4079ad6820cea54f00542cbd32c34f9207555376
15273 <ryan.mauro@vtsd.com>
2024-11-15 13:12:02 -0500

    Added a letter. Got to step 13

--- 1-2-3/apple_1.py
+++ 1-2-3/apple_1.py
@@ -3,30 +3,33 @@ import turtle as trtl
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
-pear_image = "pear.gif"
+ground_height= -200
+apple_letter_x_offset = 15
+apple_letter_y_offset = 35
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
-wn.addshape(pear_image)
 apple = trtl.Turtle()
 apple.penup()
-pear = trtl.Turtle()
 wn.bgpic("background.gif")
+
+
+
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
-def draw_apple(active_apple):
-  active_apple.shape(apple_image)
+def draw_apple():
+  apple.shape(apple_image)
+  apple.color("white")
+  apple.write("A", font=("Arial", 40, "bold"))
+  apple.goto(apple_letter_x_offset, apple_letter_y_offset)
   wn.update()
-def apple_down(active_apple):
-  x_coor = active_apple.xcor()
-  y_coor = active_apple.ycor()
-  active_apple.goto(x_coor, y_coor)
-  apple.goto(0,-150)
-def draw_pear(active_pear):
-  active_pear.shape(pear_image)
-  wn.update()
+def drop_apple():
+  apple.goto(apple.xcor(),ground_height)
+
+
+
 #-----function calls-----
-draw_apple(apple)
-draw_pear(pear)
-apple_down(apple)
+draw_apple()
+wn.onkeypress(drop_apple, "a")
+wn.listen()
 wn.mainloop()
