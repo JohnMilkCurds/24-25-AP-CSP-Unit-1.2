db5903c679360e0f841fb9004056bbaaf7ee32f7
15273 <ryan.mauro@vtsd.com>
2024-12-02 13:11:05 -0500

    Began 1-2-4 Main.py. created basic maze

---
+++ 1-2-4/Main.py
@@ -0,0 +1,48 @@
+import turtle as trtl
+
+# Initialize Variables
+wn=trtl.Screen()
+maze_painter= trtl.Turtle()
+length = 15
+path_width = 50
+# Setup Turtle
+maze_painter.speed(0)
+maze_painter.pensize(5)
+# Draw maze
+# Process:
+# Draw a line
+# Turn left
+# Increment Length
+# Repeat
+def draw_barrier():
+    maze_painter.right(90)
+    maze_painter.forward(path_width)
+    maze_painter.backward(path_width)
+    maze_painter.left(90)
+for wall in range(21):
+    maze_painter.left(90)
+    maze_painter.penup()
+    maze_painter.forward(path_width)
+    maze_painter.pendown()
+    if wall > 5:
+        draw_barrier()
+    draw_barrier()
+    maze_painter.forward(length- path_width - length/3)
+    maze_painter.forward(length)
+    length+= 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wn.listen()
+wn.mainloop()
