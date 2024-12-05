87619d109c3c09d31d5b47ba842b4d7c6d55f148
15273 <ryan.mauro@vtsd.com>
2024-12-05 13:54:48 -0500

    Began 1-2-4 Main.py. Added random door placement

--- 1-2-4/Main.py
+++ 1-2-4/Main.py
@@ -1,9 +1,10 @@
 import turtle as trtl
+import random as rand
 # Initialize Variables
 wn=trtl.Screen()
 maze_painter= trtl.Turtle()
 length = 15
-path_width = 50
+path_width = 60
 # Setup Turtle
 maze_painter.speed(0)
 maze_painter.pensize(5)
@@ -19,13 +20,16 @@ def draw_barrier():
     maze_painter.backward(path_width)
     maze_painter.left(90)
 for wall in range(21):
+    index = rand.randint(0,(length - 10))
+    rndm = rand.randint(0,(length - 10))
     maze_painter.left(90)
+    maze_painter.forward(index)
     maze_painter.penup()
     maze_painter.forward(path_width)
     maze_painter.pendown()
     if wall > 5:
         draw_barrier()
-    maze_painter.forward(length- path_width - length/3)
+    maze_painter.forward(length- path_width - index)
     maze_painter.forward(length)
     length+= 15
 wn.listen()
